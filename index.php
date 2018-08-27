610a73f2afc2a6be45e8d5cf90400853b6835643
EnderDev <42723993+EnderDev@users.noreply.github.com>
2018-08-27 01:06:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ender Bot</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+</head>
+
+<body>
+    <style>img[alt="www.000webhost.com"]{display:none;}</style>
+    <link rel="icon" type="image/x-icon" href="/enderrr_uqNqyP_256px.ico" />
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#" data-bs-hover-animate="pulse"><img src="assets/img/enderrrr.png" width="50" height="50">&nbsp; Ender Bot</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://discordapp.com/oauth2/authorize?client_id=371685425351229441&amp;scope=bot&amp;response_type=code&amp;permissions=2134207671">Invite</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav><img><img><img>
+    <main class="page landing-page">
+        <section data-aos="zoom-out-up" data-aos-duration="350" data-aos-delay="400" data-aos-once="true" class="clean-block clean-hero" style="background-image:url(&quot;assets/img/0e822a72511451636b6aaac23b26f31e.jpg&quot;);color:rgba(0,0,0,0.65);">
+            <div class="text">
+                <h2 data-bs-parallax-bg="true">Hi, I'm Ender Bot.</h2>
+                <p>A revolutionary discord bot with many amazing features.</p><a class="btn btn-outline-light btn-lg" role="button" href="https://discordapp.com/oauth2/authorize?client_id=371685425351229441&scope=bot&permissions=2134207671">Invite</a></div>
+        </section>
+        <section class="clean-block features">
+            <div class="container">
+                <div class="block-heading">
+                    <h2 class="text-info">Features</h2>
+                    <p>These are of many features Ender Bot supplies.</p>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-5 feature-box"><i class="icon-star icon"></i>
+                        <h4>Coin system</h4>
+                        <p>Everytime you chat you get 0.3 coins added to your account. If you reach a certain amount of coins you will recieve a medal.</p>
+                    </div>
+                    <div class="col-md-5 feature-box"><i class="icon-pencil icon"></i>
+                        <h4>Modern Design</h4>
+                        <p>Our design techniques are soothing to the eyes and will not confuse you at all.</p>
+                    </div>
+                    <div class="col-md-5 feature-box"><i class="icon-music-tone-alt icon"></i>
+                        <h4>Music system</h4>
+                        <p>Our music system is sleek and easy to use, with search abilities and queues, your users will never get bored.</p>
+                    </div>
+                    <div class="col-md-5 feature-box"><i class="icon-game-controller icon"></i>
+                        <h4>Fun &amp; Games</h4>
+                        <p>There are many fun commands you can try also, like !cat or !question. Try it out for yourself!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="clean-block about-us">
+            <div class="container">
+                <div class="block-heading">
+                    <h2 class="text-info">Who are we?</h2>
+                    <p>We are a small group who like making bots for you, the community.</p>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card clean-card text-center"><img class="card-img-top w-100 d-block" src="assets/img/ENDERRR.png">
+                            <div class="card-body info">
+                                <h4 class="card-title">Ender</h4>
+                                <p class="card-text">Hi, I'm the leader of Ender Bot. I just make bots.</p>
+                                <div class="icons"><a href="https://ender.site"><i class="icon-globe"></i></a><a href="https://steamcommunity.com/id/xEnderCrafterx/"><i class="icon-social-steam"></i></a><a href="https://twitter.com/xEdnerCrafterx"><i class="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card clean-card text-center"><img class="card-img-top w-100 d-block" src="assets/img/ProfilePic (1).png">
+                            <div class="card-body info">
+                                <h4 class="card-title">Geek</h4>
+                                <p class="card-text">I'm a 16 year old that loves coding, so I make Discord bots.<br></p>
+                                <div class="icons"><a href="https://gaminggeekhd.tk/"><i class="icon-globe"></i></a><a href="https://twitter.com/GamingGeekHD2"><i class="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="d-inline page-footer dark">
+        <div class="footer-copyright">
+            <p>© 2018 Ender Bot<br></p>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+</body>
+
+</html>
